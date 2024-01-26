ce448372fa2faa975e12e9f5fdfdb1383f839f2a
EdsonAsuncion <157295875+EdsonAsuncion@users.noreply.github.com>
2024-01-26 03:01:30 +0000

    carga modulo1

---
+++ Modulo1/scripts/area.py
@@ -0,0 +1,8 @@
+
+#todo lo que  que ingresas como input es un texto 
+# como yo quiero un numero,entonces int convierte el texto en numero
+base = int(input("ingrese la base:")) 
+altura = int(input("ingrese la altura:"))
+
+area= base*altura/2
+print(area)
---
+++ Modulo1/scripts/lista.py
@@ -0,0 +1,11 @@
+# crear 10 variables 
+
+
+# las listas son colecciones de datos
+    #las listas me ayudan almacenar un conjunto de valores
+listas_valores= ["gola", "xyz", 123,True, 34.5, False]
+
+print(f"impromo elemento 0 de la lista {listas_valores[0]}")
+
+
+print(f"impromo el ultimo elemento  de la lista {listas_valores[-1]}")
