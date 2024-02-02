2de070e7bb25dd539c9d16b2c9d5164ef682cba6
EdsonAsuncion <157295875+EdsonAsuncion@users.noreply.github.com>
2024-02-02 21:30:50 +0000

    cargar modulo 1

--- Modulo1/scripts/lista.py
+++ Modulo1/scripts/lista.py
@@ -6,6 +6,6 @@
 listas_valores= ["gola", "xyz", 123,True, 34.5, False]
 
 print(f"impromo elemento 0 de la lista {listas_valores[0]}")
-
+print(f" {listas_valores}")
 
 print(f"impromo el ultimo elemento  de la lista {listas_valores[-1]}")
---
+++ Modulo2/scripts/bucles.py
@@ -0,0 +1,29 @@
+# impreme 10 veces la palabra hola 
+#print('hola')
+#print('hola')
+#print('hola')
+#print('hola')
+#print('hola')
+#print('hola')
+#print('hola')
+#print('hola')
+#print('hola')
+#print('hola')
+
+#En programación hay estructuras llamadas iterativas que nos ayudan a realizar una tarea o acción multiples veces
+
+for _ in range(10):
+    print('hola')
+
+
+# bucle while --> hace una acción mientras la condicion dada sea verdad
+
+n=1 #valor inicial, inicializador 
+while n <= 10:
+    print("hola")
+    n+=1
+
+anio=2001
+while anio <= 2012:
+    print("informes del año{}".format(anio))
+    anio +=1 #aumentamos contador
